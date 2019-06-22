39dea9918f5b9bf3dc5956805b26bc9a7535583b
Anderson Buzzi <anderson.buzzi@gmail.com>
2019-06-22 17:21:09 -0300

    Ajustes e delete da categoria com exceção personalizada

--- src/main/java/br/com/buzzi/services/CategoriaService.java
+++ src/main/java/br/com/buzzi/services/CategoriaService.java
@@ -3,6 +3,7 @@ package br.com.buzzi.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.buzzi.domain.Categoria;
@@ -11,20 +12,36 @@ import br.com.buzzi.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
-	public Categoria buscar(Integer id) {
+
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		if (!obj.isPresent()) {
-			throw new ObjectNotFoundException("Objeto não encontrado. Id: " + id + ", Tipo: " + Categoria.class.getName());
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado. Id: " + id + ", Tipo: " + Categoria.class.getName());
 		}
 		return obj.orElse(null);
 	}
 
 	public Categoria insert(Categoria obj) {
 		return repo.save(obj);
+	}
+
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityViolationException("Não é possível excluir uma categoria que possui produtos");
+
+		}
 	}
 
 }
